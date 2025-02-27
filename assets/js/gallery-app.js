4cc30b2c860bdc615eced20bd4a664fafcad0b5e
Aldahir Rodríguez <aldahirrj@gmail.com>
2025-02-27 17:44:05 -0600

    catalogo y imagenes

--- assets/js/gallery-app.js
+++ assets/js/gallery-app.js
@@ -2,81 +2,88 @@ angular.module('ksmApp', [])
 .controller('MainController', function($scope) {  
 $scope.showGallery = false;
 $scope.selectedCategory = null;
+$scope.currentImageIndex = 0;
 
-// Esta es la seccion de la galeria y sus imagenes, 
-
-//Cocinas
+    
 $scope.categories = [
-
-//imagen principal que se muestra en la pagina principal
+    // Cocinas
 {
     id: 'cocinas',
     name: 'Cocinas',
     thumbnail: './assets/img/cocina.jpg',
     description: 'Nuestras cocinas están diseñadas para combinar funcionalidad y estética, adaptándose a tu estilo y necesidades.',
     images: [
-
-//este es el formato para ir agregando las imagenes que queramos, solo copien y peguenlo.. (solo coloquen la imagen dos veces en la miniatura)
 {
-    full: './assets/img/vestidor.jpg',
-    thumb: './assets/img/vestidor.jpg',
-    title: 'Cocina de estilo Tradicional ',
-    description: 'Diseño elegante con acabados de alta calidad y funcionalidad integrada etcetc.'
-    },
-{
-    full: '.assets/img/uso2.jpg',
+    full: './assets/img/cocina.jpg',
     thumb: './assets/img/cocina.jpg',
-    title: 'Cocina de estilo Tradicional 2maxplus300 ',
-    description: 'otra descripcion de la cocina c.'
-    },
-]
-//Closets
+    title: 'Cocina de estilo Tradicional',
+    description: 'Diseño elegante con acabados de alta calidad y funcionalidad integrada.'
+        },
+{
+    full: './assets/img/Cocina5.jpg',
+    thumb: './assets/img/Cocina5.jpg',
+    title: 'Cocina de estilo Tradicional',
+    description: 'Diseño elegante con acabados de alta calidad y funcionalidad integrada.'
+        },
+{
+    full: './assets/img/Cocina4.jpg',
+    thumb: './assets/img/Cocina4.jpg',
+    title: 'Cocina de estilo Tradicional',
+    description: 'Diseño elegante con acabados de alta calidad y funcionalidad integrada.'
+        },
+{
+    full: './assets/img/Cocina3.jpg',
+    thumb: './assets/img/Cocina3.jpg',
+    title: 'Cocina de estilo Tradicional 2',
+    description: 'Otra descripción de la cocina.'
+}]
 },
-    {
+
+
+// Closets
+{
     id: 'closets',
     name: 'Closets',
     thumbnail: './assets/img/closet.jpg',
     description: 'Creamos closets personalizados que aprovechan al máximo el espacio y se adaptan a tus necesidades de almacenamiento.',
     images: [
-{
-    full: 'Carpinteria\assets\img\vestidor.jpg',
-    thumb: '',
-    title: 'Closet Modular',
-    description: 'Texto de la descripcion de la imagen.'
-    },
-]
-//vestidores
-},
     {
+    full: './assets/img/vestidor.jpg', // Corregido: cambiado a formato consistente
+    thumb: './assets/img/vestidor.jpg', // Añadido thumb que faltaba
+    title: 'Closet Modular',
+    description: 'Texto de la descripción de la imagen.'
+}
+]
+},
+// Vestidores
+{
     id: 'vestidores',
     name: 'Vestidores',
     thumbnail: './assets/img/baño.jpg',
     description: 'Diseñamos vestidores elegantes y funcionales que transformarán tu experiencia diaria.',
     images: [
-{
-    full: './assets/img/gallery/vestidores/vestidor1.jpg',
-    thumb: '',
-    title: 'Titulo de la imagen',
-    description: 'Texto de la descripcion de la imagen.'
-    },
-]
-//Baños
-},
     {
+    full: './assets/img/gallery/vestidores/vestidor1.jpg',
+    thumb: './assets/img/gallery/vestidores/vestidor1.jpg', // Añadido thumb que faltaba
+    title: 'Título de la imagen',
+    description: 'Texto de la descripción de la imagen.'
+}]
+},
+// Baños
+{
     id: 'muebles-bano',
     name: 'Muebles de baño',
     thumbnail: './assets/img/baño.jpg',
     description: 'Nuestros muebles de baño combinan durabilidad, resistencia a la humedad y diseño contemporáneo.',
     images: [
-{
+    {
     full: './assets/img/gallery/banos/bano1.jpg',
     thumb: './assets/img/gallery/banos/thumbs/bano1.jpg',
-    title: 'Titulo de la imagen',
-    description: 'Texto de la descripcion de la imagen.'
-    },
-]
-//Muebles especiales
+    title: 'Título de la imagen',
+    description: 'Texto de la descripción de la imagen.'
+}]
 },
+// Muebles especiales
 {
     id: 'muebles-especiales',
     name: 'Muebles especiales',
@@ -85,44 +92,83 @@ $scope.categories = [
     images: [
 {
     full: './assets/img/gallery/especiales/especial1.jpg',
-    thumb: '',
-    title: 'Titulo de la imagen',
-    description: 'Texto de la descripcion de la imagen.'
-    },
-
-]
+    thumb: './assets/img/gallery/especiales/especial1.jpg', 
+    title: 'Título de la imagen',
+    description: 'Texto de la descripción de la imagen.'
     }
+]}
 ];
 
+// Método para mostrar la galería de una categoría
 
-// Este apartado lo hizo GPT para implementar lenguaje de YETTI al trabajo, ya que no sabia que hacia muy bien. 
+    $scope.showCategoryGallery = function(category) {
+        $scope.selectedCategory = category;
+        $scope.showGallery = true;
+        $scope.currentImageIndex = 0; 
 
-$scope.showCategoryGallery = function(category) {
-    $scope.selectedCategory = category;
-    $scope.showGallery = true;
-    setTimeout(function() {
-window.galleryTabber = new Yetii({
-        id: 'gallery-container-' + category.id
+        // Mostrar solo la primera imagen
+
+        setTimeout(function() {
+            updateVisibleImage($scope.currentImageIndex);
+        }, 100);
+    };
+
+    // Método para cambiar a la siguiente imagen
+
+    $scope.nextImage = function() {
+        if ($scope.selectedCategory && $scope.selectedCategory.images.length > 0) {
+            $scope.currentImageIndex = ($scope.currentImageIndex + 1) % $scope.selectedCategory.images.length;
+            updateVisibleImage($scope.currentImageIndex);
+        }
+    };
+
+    // Método para cambiar a una imagen específica
+    $scope.showImage = function(index) {
+        if ($scope.selectedCategory && index >= 0 && index < $scope.selectedCategory.images.length) {
+            $scope.currentImageIndex = index;
+            updateVisibleImage($scope.currentImageIndex);
+        }
+    };
+
+    // Función auxiliar para actualizar la imagen visible
+
+    function updateVisibleImage(index) {
+
+        var tabs = document.querySelectorAll('#gallery-container-' + $scope.selectedCategory.id + ' .tab');
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].style.display = 'none';
+            tabs[i].classList.remove('active-tab');
+        }
+
+        if (tabs[index]) {
+            tabs[index].style.display = 'block';
+            tabs[index].classList.add('active-tab');
+        }
+
+       //Fuccion que activa la miniatura
+
+        var navLinks = document.querySelectorAll('#gallery-container-' + $scope.selectedCategory.id + '-nav a');
+        for (var j = 0; j < navLinks.length; j++) {
+            navLinks[j].classList.remove('active');
+        }
+        if (navLinks[index]) {
+            navLinks[index].classList.add('active');
+        }
+    }
+
+    // Back para volver a las categorias
+
+    $scope.backToCategories = function() {
+        $scope.showGallery = false;
+        $scope.selectedCategory = null;
+        $scope.currentImageIndex = 0;
+    };
 });
 
-// Mostrar la primera imagen
-
-document.querySelector('#gallery-container-' + category.id + ' .tab').style.display = 'block';
-    }, 100);
-};
-
-// Esta funcion sirve para poder salir y entrar de la galeria 
-
-$scope.backToCategories = function() {
-    $scope.showGallery = false;
-    $scope.selectedCategory = null;
-};
-});
-
-// Para que inicie Angular
+// Iniciar Angular
 
 angular.element(document).ready(function() {
-if (!angular.element(document.body).scope()) {
-    angular.bootstrap(document, ['ksmApp']);
-}
+    if (!angular.element(document.body).scope()) {
+        angular.bootstrap(document, ['ksmApp']);
+    }
 });
